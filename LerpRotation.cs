1801c9465f071791cc13606af24a9773eed0fe4e
Nick <nickthwang@gmail.com>
2017-04-27 16:40:27 -0500

    Create LerpRotation.cs

---
+++ LerpRotation.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections;
+
+public class LerpRotation : MonoBehaviour {
+	// use this script to control a rotation of an object, to turn and face a target object
+
+	Quaternion targetRotation;
+
+	public Transform target;			// target object 
+	public float speed = 0.1F;			// speed scaling factor
+
+	bool rotating = false;				// toggles the rotation, after targeting, toggle true, false after arrives
+
+	float rotationTime; // when rotationTime == 1, will have rotated to our target
+
+	void Update() {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Vector3 relativePosition = target.position - transform.position;
+			targetRotation = Quaternion.LookRotation (relativePosition);
+			rotating = true;
+			rotationTime = 0;
+		}
+		if (rotating) {
+			rotationTime += Time.deltaTime * speed;
+			transform.rotation = Quaternion.Lerp (transform.rotation, targetRotation, rotationTime);
+		}
+		if (rotationTime > 1) {
+			rotating = false;
+		}
+	}
+}
+
+
+/// Rotating FROM A ----------> B
+///                     .5
+/// 			  0. ----------> 1.
